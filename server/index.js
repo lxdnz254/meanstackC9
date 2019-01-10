3f01434b937f0c98e6c072dae42afa73b6537b44
lxdnz254 <lxdnz34@gmail.com>
2019-01-10 03:42:28 +0000

    Removed mongo initialise

--- server/index.js
+++ server/index.js
@@ -31,23 +31,10 @@ app.use('/api/v1', require('./api')(wagner));
 // file.
 app.use(express.static('myApp/www/', { maxAge: 4 * 60 * 60 * 1000 /* 2hrs */ }));
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
 
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
-
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+	var port = server.address().port;
+	console.log("App now running on port", port);
 });
+
