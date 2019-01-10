44ee6fe690b18bfc82121cb4a49ce4624c9404c0
lxdnz254 <lxdnz34@gmail.com>
2019-01-10 00:22:16 +0000

    Added mongo connect

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var wagner = require('wagner-core');
+var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
 
 require('./models')(wagner);
 require('./dependencies')(wagner);
 
 var app = express();
+
+app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
 	res.append('Access-Control-Allow-Origin', req.headers.origin || '*');
@@ -27,6 +31,23 @@ app.use('/api/v1', require('./api')(wagner));
 // file.
 app.use(express.static('../myApp/www/', { maxAge: 4 * 60 * 60 * 1000 /* 2hrs */ }));
 
-app.listen(process.env.PORT);
-console.log('Listening on port %s', process.env.PORT);
-console.log('Brodcasting on IP %s', process.env.IP);
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
